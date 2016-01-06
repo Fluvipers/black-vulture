36db4537257a47d3d626692464f3fa558307cdbb
josediaz16 <jldiaz1612@gmail.com>
2016-01-06 15:49:12 -0500

    create unit test for line to avoid two lines with the same name

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,3 +1,5 @@
 class Line < ActiveRecord::Base
+  has_many :facts, dependent: :destroy
   validates :name, presence: true
+  validates :name, uniqueness: { case_sensitive: false, message: "There is already a line with that name"}
 end
