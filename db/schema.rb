d0c091491411f52d8047430e7838943d440efbc6
manvedu <mavelandiad@gmail.com>
2016-01-26 13:24:05 -0500

    enable_hstore

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20160126181632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
 
   create_table "facts", force: :cascade do |t|
     t.integer  "line_id"
